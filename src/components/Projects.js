c58abaf194c330e8ded551d5bfffe793a27c2e3c
hannan-emily <emilyhannan@gmail.com>
2018-04-05 14:58:45 -0700

    finished updating content, links and logos for project:chasing carmen sandiego

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -57,24 +57,20 @@ class Projects extends Component {
             Test your geographic knowledge in this two player HTML5 canvas game!</p>
 
             <Col s={6}>
-              <a className='button btn' href="https://pnw-overland.herokuapp.com" target="_blank">LIVE GAME</a>
+              <a className='button btn' href="https://hannan-emily.github.io/ChasingCarmenSanDiego/" target="_blank">LIVE GAME</a>
             </Col>
 
             <Col s={6}>
-              <a className='button btn' href="https://github.com/hannan-emily/pnw-overland" target="_blank">GITHUB</a>
+              <a className='button btn' href="https://github.com/hannan-emily/ChasingCarmenSanDiego" target="_blank">GITHUB</a>
             </Col>
 
             <Row >
               <div className='tech-used-parent'>
                 <h5 className='tech-used'>TECHNOLOGIES USED&nbsp;&nbsp;</h5>
-                <img className="logo-tiny" src="./img/logo-node-square50.png" alt="" />
-                <img className="logo-tiny" src="./img/logo-express-square50.png" alt="" />
-                <h6 className="logo-tiny">EJS &nbsp;&nbsp;&nbsp;</h6>
-                <h6 className="logo-tiny">POSTGRESQL&nbsp;&nbsp;&nbsp;</h6>
-                <h6 className="logo-tiny">BCRYPT&nbsp;&nbsp;&nbsp;</h6>
-                <h6 className="logo-tiny">ASYNC&nbsp;&nbsp;&nbsp;</h6>
+                <img className="logo-tiny" src="./img/logo-jquery-square50.png" alt="" />
+                <img className="logo-tiny" src="./img/logo-js-square50.png" alt="" />
+                <h6 className="logo-tiny">HTML5 CANVAS&nbsp;&nbsp;&nbsp;</h6>
                 <h6 className="logo-tiny">MATERIALIZE&nbsp;&nbsp;&nbsp;</h6>
-                <h6 className="logo-tiny">GOOGLE MAPS API&nbsp;&nbsp;&nbsp;</h6>
               </div>
             </Row>
 
